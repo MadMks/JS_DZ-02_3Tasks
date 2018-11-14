07df74767859da4828d9dd1bd9e355d4ea9e6b98
Makc <mks59k@gmail.com>
2018-11-14 12:58:07 +0200

    add function showCarInfo()

--- js/task1.js
+++ js/task1.js
@@ -4,3 +4,13 @@ var car = {
     year: 2000,
     averageSpeed: 70
 };
+
+function showCarInfo(obj) {
+    var str = "Производитель: " + obj.manufacturer + "\n"
+        + "Модель: " + obj.model + "\n"
+        + "Год выпуска: " + obj.year + "\n"
+        + "Средняя скорость: " + obj.averageSpeed;
+    alert(str);
+}
+
+showCarInfo(car);
