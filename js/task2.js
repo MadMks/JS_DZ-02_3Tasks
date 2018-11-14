02ff62e1900dbd78bae6f864c77ce132606673fe
Makc <mks59k@gmail.com>
2018-11-14 23:12:17 +0200

    Добавлен список TODO.

--- js/task2.js
+++ js/task2.js
@@ -8,17 +8,64 @@ var fractionAnswer = {};
 function showFraction(obj) {
     return obj.numerator + "/" + obj.denomerator;
 }
+
+
 // Вычисление суммы.
-function sum(obj1, obj2) {  // TODO реализовать вычисление.
+function sum(obj1, obj2) {  
+    // TODO: реализовать вычисление суммы.
     return obj1;
 }
-// Показать вычисление суммы.
-function showComputeSum() {
-    fractionAnswer = sum(fractionOne, fractionOne);
+// Вычисление суммы.
+function sum(obj1, obj2) {  
+    // TODO: реализовать вычисление вычитания.
+    return obj1;
+}
+// Вычисление суммы.
+function sum(obj1, obj2) {  
+    // TODO: реализовать вычисление умножения.
+    return obj1;
+}
+// Вычисление суммы.
+function sum(obj1, obj2) {  
+    // TODO: реализовать вычисление деления.
+    return obj1;
+}
+// Вычисление суммы.
+function sum(obj1, obj2) {  
+    // TODO: реализовать вычисление сокращения.
+    return obj1;
+}
 
+
+// Показать вычисление.
+function showCompute(sign) {
+
+    switch (sign) {
+        case '+':
+            fractionAnswer = sum(fractionOne, fractionOne);
+            break;
+        case '-':
+            // TODO
+            break;
+        case '*':
+            // TODO
+            break;
+        case '/':
+            // TODO
+            break;
+        case '?????':   // TODO: сокращение дроби.
+            // TODO
+            break;
+    
+        default:
+            alert("[error] недопустимый знак вычисения!")
+            return;
+    }
+    
+    // Выводим формулу и результат вычисления.
     alert(
         showFraction(fractionOne) 
-        + " + " 
+        + " " + sign + " " 
         + showFraction(fractionOne) 
         + " = " 
         + showFraction(fractionAnswer)
