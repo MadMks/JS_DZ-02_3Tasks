dbd82a9428c3c415d3420fac2cd2862ded166d22
Makc <mks59k@gmail.com>
2018-11-14 20:23:01 +0200

    Реализовано вычислении времени пройденного пути.

--- js/task1.js
+++ js/task1.js
@@ -8,21 +8,28 @@ var car = {
 function showCarInfo(obj) {
     var str = "Производитель: " + obj.manufacturer + "\n"
         + "Модель: " + obj.model + "\n"
-        + "Год выпуска: " + obj.year + "\n"
-        + "Средняя скорость: " + obj.averageSpeed;
+        + "Год выпуска: " + obj.year + " г\n"
+        + "Средняя скорость: " + obj.averageSpeed + " км/ч";
     alert(str);
 }
 
 // showCarInfo(car);
 
 function showTravelTime() {
-    var time = computeTravelTime(/**/)  // TODO 1 length
 
-    alert("Расстояние: " + length + "\n"
-        + "Время для преодоления: " + time);
+    var length = prompt("Введите расстояние (км)", "");
+
+    var time = computeTravelTime(length)
+
+    alert("Средняя скорость: " + car.averageSpeed + " км/ч\n"
+        + "Расстояние: " + length + " км\n"
+        + "Время для преодоления: " + time + " ч");
 }
 
 function computeTravelTime(length) {
-    // TODO расчет
-    return length; // TODO возвратить правильную переменную
+    return Math.round(
+        (length / car.averageSpeed) * 100
+        ) / 100;
 }
+
+// TODO каждые 4 часа дороги водителю необходимо делать перерыв на 1 час.
